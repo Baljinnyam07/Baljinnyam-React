9b2bd24c17e2ee06f610f35563759355837ce38a
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-01-20 17:04:48 +0800

    commit

---
+++ blog/blog/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { Card } from "react-bootstrap";
+
+export default function Home() {
+  const articles = [
+    { image: "https://news.mn/wp-content/uploads/2023/01/News114-1-530x330.jpg", title: "Ш.ЭРДЭНЭЖАРГАЛ, Б.НАРАНХҮҮ, М.ДӨЛГӨӨН НАРЫГ ХОРЬЖЭЭ" },{ image: "https://news.mn/wp-content/uploads/2023/01/Mongol-Khan-2-530x330.jpg", title: "“МОНГОЛ ХААН” ДЭЛХИЙН ТАЙЗЫГ ТАМГАЛНА" },{ image: "https://news.mn/wp-content/uploads/2023/01/1251-1-530x330.jpg", title: "" },{ image: "https://news.mn/wp-content/uploads/2023/01/128340938_ze_davos_1-530x330.jpg", title: "" }}
+  return (
+    <main>
+      <div className="container">
+        <div className="row">
+          {articles.map((article) => {
+            <div className="col-md-3 col-sm-6 col-12">
+                <Card title={article.title} image={article.image}/>
+              </div>
+          }
+              
+          )}
+          
+        </div>
+      </div>
+    </main>
+  );
+}
