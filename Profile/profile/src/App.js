2588e36bf979613277b26cd58ba6c0dceff8701f
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-02-10 17:31:47 +0800

    commit

---
+++ Profile/profile/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Box from "@mui/material/Box";
+import MiniDrawer from './components/Main';
+
+function App() {
+  return <>
+  <Box sx={{background:"rgb(246, 247, 255)"}}>
+    <MiniDrawer/>
+    
+    
+  </Box>
+  </>;
+}
+
+export default App;
---
+++ Profile/profile/src/components/SideBar.jsx
@@ -0,0 +1,101 @@
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+import { useTheme } from "@mui/material/styles";
+
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import { Drawer, IconButton } from "@mui/material";
+import { DrawerHeader } from "./main";
+import { useState } from "react";
+
+export default function SideBar() {
+  const [open, setOpen] = useState(false);
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const theme = useTheme();
+  return (
+    <Drawer variant="permanent" open={open}>
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === "rtl" ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {["Profile", "Dashboard", "E-commerce", "Users", "Documentation"].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+      <Divider />
+      <List>
+        {[
+          "Core",
+          "Tables",
+          "UI Elements",
+          "Forms",
+          "Charts",
+          "Grid",
+          "Maps",
+          "Extra",
+          "Menu Levels",
+        ].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Drawer>
+  );
+}
