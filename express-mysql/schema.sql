dabd44928e88c34e366377c3ff3d2d6876599f69
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-03-06 17:20:20 +0800

    commit

--- express-mysql/schema.sql
+++ express-mysql/schema.sql
@@ -19,3 +19,46 @@ CREATE TABLE
         createdAt datetime,
         PRIMARY KEY (id)
     );
+
+-- TODOCATEGORY
+
+CREATE TABLE
+    todoCategory (
+        id int unsigned NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255),
+        PRIMARY KEY (id)
+    );
+
+--TODO
+
+CREATE TABLE
+    todo (
+        id int unsigned NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255),
+        isDone BOOLEAN DEFAULT false,
+        todoCategoryId int UNSIGNED,
+        PRIMARY KEY(id),
+        FOREIGN KEY(todoCategoryId) REFERENCES todoCategory(id)
+    );
+
+INSERT INTO todoCategory VALUES(NULL,'Must do');
+
+INSERT INTO todo values (NULL,'Brush teeth', false, 1);
+
+-- 1.todoCategoryId ustgah
+
+ALTER Table todo drop constraint todo_ibfk_1;
+
+-- 2.
+
+alter table
+    todo drop todoCategoryId -- 1.todoCategoryId nemeh
+ALTER TABLE todo
+ADD
+    COLUMN todoCategoryId INT unsigned;
+
+-- 2.
+
+ALTER TABLE doto
+ADD
+    FOREIGN KEY (todoCategoryId) REFERENCES todoCategory(id);
